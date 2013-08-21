6997d2ac2dedb1c691af850b80480ee988d0bfb3
Mouse <murahoid@gmail.com>
2013-08-21 22:00:10 +0300

    routes3

--- lib/config/custom-routes.rb
+++ lib/config/custom-routes.rb
@@ -1,5 +1,3 @@
 # Here you can override or add to the pages in the core website
 
-Alaveteli::Application.routes.draw do |map|
-   # match '/adminbootstraptheme/javascripts/admin.js' => 'general#admin_js', :as => :admin_js 
-end
+
