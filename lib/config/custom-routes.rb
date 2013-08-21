9f3e36782eb572428e39c630ba7fbbcd409f9422
Mouse <murahoid@gmail.com>
2013-08-21 21:53:26 +0300

    routes2

--- lib/config/custom-routes.rb
+++ lib/config/custom-routes.rb
@@ -1,13 +1,5 @@
 # Here you can override or add to the pages in the core website
 
 Alaveteli::Application.routes.draw do |map|
-    # brand new controller example
-    map.with_options :controller => 'general' do |general|
-        general.mycontroller '/mycontroller', :action => 'mycontroller'
-    end    
-    
-    # Additional help page example
-    map.with_options :controller => 'help' do |help|
-        help.help_out '/help/help_out', :action => 'help_out'
-    end
+   # match '/adminbootstraptheme/javascripts/admin.js' => 'general#admin_js', :as => :admin_js 
 end
