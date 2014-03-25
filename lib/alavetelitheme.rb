ca178bec220f78d51939d2a8932386cd636bf5a3
n-mouse <murahoid@gmail.com>
2014-03-25 12:32:01 +0200

    request list rollback

--- lib/alavetelitheme.rb
+++ lib/alavetelitheme.rb
@@ -33,11 +33,3 @@ end
 # something unique (e.g. yourtheme-custom-routes.rb":
 $alaveteli_route_extensions << 'custom-routes.rb'
 
-# Prepend the asset directories in this theme to the asset path:
-['stylesheets', 'images', 'javascripts'].each do |asset_type|
-    theme_asset_path = File.join(File.dirname(__FILE__),
-                                 '..',
-                                 'assets',
-                                 asset_type)
-    Rails.application.config.assets.paths.unshift theme_asset_path
-end
