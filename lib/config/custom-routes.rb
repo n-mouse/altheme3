1d9b544a8f4be190532a165d9c36c048de92741a
n-mouse <murahoid@gmail.com>
2015-09-28 02:16:08 +0300

    kd;jfdjf

--- lib/config/custom-routes.rb
+++ lib/config/custom-routes.rb
@@ -3,6 +3,10 @@ Alaveteli::Application.routes.draw do
 
     resources :publications, :only => [:index, :show]
     
+    get "stories", to: "publications#stories"
+    get "videos", to: "publications#videos"
+    get "news", to: "publications#news"
+    
     scope '/admin', :as => 'admin' do
         resources :publications, :controller => 'admin_publications'
     end
